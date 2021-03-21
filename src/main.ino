09208b06cb5b45c94242bf374fc97f87f4728144
Edouard Menayde <edouard@menayde.fr>
2021-03-21 15:27:51 +0100

    wip : switch to homebridge

--- src/main.ino
+++ src/main.ino
@@ -4,165 +4,49 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <WS2812FX.h>
-#include <EasyButton.h>
-#include <arduino_homekit_server.h>
 
 #include "secrets.h"
-
-//access the config defined in C code
-
-extern "C" homekit_server_config_t accessory_config;
-extern "C" homekit_characteristic_t cha_on;
-extern "C" homekit_characteristic_t cha_bright;
-extern "C" homekit_characteristic_t cha_sat;
-extern "C" homekit_characteristic_t cha_hue;
 
 char _id[12];
 char _hostname[18] = HOSTNAME_PREFIX;
 
 WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
-WiFiClient wifi;
-
-EasyButton reset_homekit(BUTTON_PIN);
-
-// Maintained state
-bool received_sat = false;
-bool received_hue = false;
-
+WiFiServer server(80);
+WiFiClient client;
 bool is_on = false;
-float current_brightness = 100.0;
-float current_sat = 0.0;
-float current_hue = 0.0;
-int rgb_colors[3] = {0, 0, 0};
+int red, blue, green = 0;
+String hexColor;
 
-void set_on(const homekit_value_t v)
-{
-  bool on = v.bool_value;
-  cha_on.value.bool_value = on; //sync the value
-
-  if (on) {
-    is_on = true;
-    Serial.println("On");
-  }
-  else {
-    is_on = false;
-    Serial.println("Off");
-  }
-
-  updateColor();
+void start() {
+  ws2812fx.start();
+  is_on = true;
 }
 
-void set_hue(const homekit_value_t v)
-{
-  Serial.println("set_hue");
-  float hue = v.float_value;
-  cha_hue.value.float_value = hue; //sync the value
-
-  current_hue = hue;
-  received_hue = true;
-
-  updateColor();
+void stop() {
+  ws2812fx.stop();
+  is_on = false;
 }
 
-void set_sat(const homekit_value_t v)
-{
-  Serial.println("set_sat");
-  float sat = v.float_value;
-  cha_sat.value.float_value = sat; //sync the value
-
-  current_sat = sat;
-  received_sat = true;
-
-  updateColor();
+void setColor(String str) {
+  String hexString = str.substring(9, 15);
+  long number = (long)strtol(&hexString[0], NULL, 16);
+  red = number >> 16;
+  green = number >> 8 & 0xFF;
+  blue = number & 0xFF;
+  ws2812fx.setColor(red, blue, green);
 }
 
-void set_bright(const homekit_value_t v)
-{
-  Serial.println("set_bright");
-  int bright = v.int_value;
-  cha_bright.value.int_value = bright; //sync the value
+float computeBrightness() {
+  float r = roundf(red / 2.55);
+  float g = roundf(green / 2.55);
+  float b = roundf(blue / 2.55);
+  float brightness = _max(r, g);
+  brightness = _max(brightness, b);
 
-  current_brightness = bright;
-
-  updateColor();
+  return brightness;
 }
 
-void updateColor()
-{
-  if (is_on)
-  {
-    if (received_hue && received_sat) {
-      HSV2RGB(current_hue, current_sat, current_brightness);
-      received_hue = false;
-      received_sat = false;
-    }
-
-    int b = map(current_brightness, 0, 100, 75, 255);
-    Serial.println(b);
-
-    ws2812fx.setBrightness(b);
-    ws2812fx.setColor(rgb_colors[0], rgb_colors[1], rgb_colors[2]);
-    ws2812fx.start();
-  }
-  else {
-    ws2812fx.stop();
-  }
-}
-
-// void mqttProcessJson(char *message)
-// {
-//     StaticJsonDocument<_mqtt_buffer_size> jsonDoc;
-//     auto error = deserializeJson(jsonDoc, message);
-
-//     if (error)
-//     {
-//         return;
-//     }
-
-//     if (jsonDoc.containsKey("state"))
-//     {
-//         if (strcmp(jsonDoc["state"], "ON") == 0)
-//         {
-//             ws2812fx.start();
-//         }
-//         else if (strcmp(jsonDoc["state"], "OFF") == 0)
-//         {
-//             ws2812fx.stop();
-//         }
-//     }
-
-//     if (jsonDoc.containsKey("effect"))
-//     {
-//         ws2812fx.setMode(effectNumber(jsonDoc["effect"]));
-//     }
-
-//     if (jsonDoc.containsKey("color"))
-//     {
-//         red = jsonDoc["color"]["r"];
-//         green = jsonDoc["color"]["g"];
-//         blue = jsonDoc["color"]["b"];
-//         ws2812fx.setColor(getColorInt());
-//     }
-
-//     if (jsonDoc.containsKey("white_value"))
-//     {
-//         white = jsonDoc["white_value"];
-//         ws2812fx.setColor(getColorInt());
-//     }
-
-//     if (jsonDoc.containsKey("brightness"))
-//     {
-//         ws2812fx.setBrightness(jsonDoc["brightness"]);
-//     }
-
-//     if (jsonDoc.containsKey("speed"))
-//     {
-//         ws2812fx.setSpeed(jsonDoc["speed"]);
-//     }
-// }
-
-void setupOTA()
-{
+void setupOTA() {
     ArduinoOTA.onStart([]() {
         ws2812fx.stop();
         String type;
@@ -195,8 +79,7 @@ void setupOTA()
     Serial.println("OTA: ready");
 }
 
-void setupWifi()
-{
+void setupWifi() {
     WiFi.hostname(_hostname);
     WiFi.begin(WIFI_SSID, WIFI_PW);
     Serial.printf("Connecting to %s (%s) ", WIFI_SSID, _id);
@@ -218,20 +101,72 @@ void setupFX()
     ws2812fx.init();
     ws2812fx.setBrightness(100);
     ws2812fx.setSpeed(1024);
-    ws2812fx.setColor(rgb_colors[0], rgb_colors[1], rgb_colors[2]);
+    ws2812fx.setColor(red, green, blue);
     ws2812fx.setMode(FX_MODE_STATIC);
     ws2812fx.start();
     Serial.println("FX: ready");
 }
 
-void homekit_setup()
-{
-  cha_on.setter = set_on;
-  cha_bright.setter = set_bright;
-  cha_sat.setter = set_sat;
-  cha_hue.setter = set_hue;
+void server_loop() {
+  WiFiClient client = server.available();
 
-  arduino_homekit_setup(&accessory_config);
+  if (!client) {
+    return;
+  }
+
+  while (client.connected() && !client.available()) {
+    delay(1);
+  }
+
+  String request = "";
+
+  if (client) {
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+
+        if (request.length() < 100) {
+          request += c;
+        }
+
+        if (c == '\n') {
+          Serial.print("Request: ");
+          Serial.println(request);
+
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println();
+
+          if (request.indexOf("on") > 0) {
+            start();
+          }
+
+          if (request.indexOf("off") > 0) {
+            stop();
+          }
+
+          if (request.indexOf("set") > 0) {
+            setColor(request);
+          }
+
+          if (request.indexOf("status") > 0) {
+            client.println(is_on);
+          }
+
+          if (request.indexOf("color") > 0) {
+            client.println(hexColor);
+          }
+
+          if (request.indexOf("bright") > 0) {
+            client.println(computeBrightness());
+          }
+
+          delay(1);
+          client.stop();
+        }
+      }
+    }
+  }
 }
 
 void setup()
@@ -248,9 +183,6 @@ void setup()
     setupWifi();
     setupOTA();
     setupFX();
-    homekit_setup();
-
-    reset_homekit.onPressed(homekit_storage_reset);
 
     Serial.println();
 }
@@ -259,75 +191,11 @@ void loop()
 {
     ArduinoOTA.handle();
     ws2812fx.service();
-    arduino_homekit_loop();
-    reset_homekit.read();
+    server_loop();
 }
 
-void HSV2RGB(float h, float s, float v)
-{
-
-  int i;
-  float m, n, f;
-
-  s /= 100;
-  v /= 100;
-
-  if (s == 0)
-  {
-    rgb_colors[0] = rgb_colors[1] = rgb_colors[2] = round(v * 255);
-    return;
-  }
-
-  h /= 60;
-  i = floor(h);
-  f = h - i;
-
-  if (!(i & 1))
-  {
-    f = 1 - f;
-  }
-
-  m = v * (1 - s);
-  n = v * (1 - s * f);
-
-  switch (i)
-  {
-
-  case 0:
-  case 6:
-    rgb_colors[0] = round(v * 255);
-    rgb_colors[1] = round(n * 255);
-    rgb_colors[2] = round(m * 255);
-    break;
-
-  case 1:
-    rgb_colors[0] = round(n * 255);
-    rgb_colors[1] = round(v * 255);
-    rgb_colors[2] = round(m * 255);
-    break;
-
-  case 2:
-    rgb_colors[0] = round(m * 255);
-    rgb_colors[1] = round(v * 255);
-    rgb_colors[2] = round(n * 255);
-    break;
-
-  case 3:
-    rgb_colors[0] = round(m * 255);
-    rgb_colors[1] = round(n * 255);
-    rgb_colors[2] = round(v * 255);
-    break;
-
-  case 4:
-    rgb_colors[0] = round(n * 255);
-    rgb_colors[1] = round(m * 255);
-    rgb_colors[2] = round(v * 255);
-    break;
-
-  case 5:
-    rgb_colors[0] = round(v * 255);
-    rgb_colors[1] = round(m * 255);
-    rgb_colors[2] = round(n * 255);
-    break;
-  }
+float max3(float a, float b, float c) {
+  float m = _max(a, b);
+  m = _max(m, c);
+  return m;
 }
